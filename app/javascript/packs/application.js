c88dc84b6a4893fc00e7963b40826913e9d55e06
Leo Chaput <leo.chaput@hotmail.fr>
2019-12-21 16:53:02 +0100

    creation of a scrolling effect on navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import "bootstrap";
 
+import { navScrollEffect } from "../components/navbar_scroll_effect";
 import { navOnClick } from "../components/navbar_on_click";
 import { disableAnimAos } from "../components/disable_aos";
 import { aosEffect } from "../plugins/aos";
 
+navScrollEffect();
 navOnClick();
 disableAnimAos();
 aosEffect();
