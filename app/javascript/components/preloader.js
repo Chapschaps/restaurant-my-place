4306f1b7fd1aa3efeabea845ba15bea45932249a
Leo Chaput <leo.chaput@hotmail.fr>
2020-02-03 19:31:01 +0100

    add a preloader

---
+++ app/javascript/components/preloader.js
@@ -0,0 +1,18 @@
+const preloader = () => {
+  window.addEventListener('load', () => {
+    const preload = document.querySelector('.preloader');
+    // var op = 1;  // initial opacity
+    // var timer = setInterval(function () {
+    //     if (op <= 0.1){
+    //         clearInterval(timer);
+    //         preload.style.display = 'none';
+    //     }
+    //     preload.style.opacity = op;
+    //     preload.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    //     op -= op * 0.1;
+    // }, 50);
+    preload.style.display = "none";
+  });
+}
+
+export { preloader };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,14 @@
 import "bootstrap";
 
+import { preloader } from "../components/preloader";
 import { navScrollEffect } from "../components/navbar_scroll_effect";
 import { navOnClick } from "../components/navbar_on_click";
 import { disableAnimAos } from "../components/disable_aos";
 import { aosEffect } from "../plugins/aos";
 
+preloader();
 navScrollEffect();
 navOnClick();
 disableAnimAos();
 aosEffect();
+
